624a87e7d1f98deea50ac46e2cc1a848b7bcbc77
santoshi342 <santoshiyuvasoft342@gmail.com>
2022-06-07 16:05:12 +0530

    first commit

---
+++ src/components/logout.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+
+import { getLogout } from  '../redux/actions/login'
+
+
+function Logout() {
+
+	const navigate = useNavigate();
+	const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    console.log(333)
+    fetchData()
+   
+   
+  },[])
+
+   const fetchData = async() => {
+      await dispatch(getLogout())
+      await localStorage.removeItem('user-info')
+      await navigate("/login")
+    }
+
+  return (
+     <div>....</div>
+    )
+}
+
+export default Logout;
